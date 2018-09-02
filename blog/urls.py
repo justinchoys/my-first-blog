576ae4026bac994e403d21b3e9b5f3e3c58c1514
jchoys <justinchoys@gmail.com>
2018-09-02 19:54:29 -0400

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), #This pattern will tell Django that views.post_list is the right place to go if someone enters your website at the 'http://127.0.0.1:8000/' address.
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,17 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
+#for single post page handling and error for getting objects
+from django.shortcuts import get_object_or_404
+
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	#take request and render it using appropriate html file
 	#'post' is name referring to post variable in function while inside HTML file 
     return render(request, 'blog/post_list.html', {'posts' : posts})
 
+def post_detail(request, pk): #post_detail with catch the pk variable passed via url
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post' : post})
+	
